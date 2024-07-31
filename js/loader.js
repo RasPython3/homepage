1e03fb30bae00e49af738918b93425f61ec679b8
RasPython3 <87003300+RasPython3@users.noreply.github.com>
2024-07-31 19:53:00 +0900

    Change url

--- js/loader.js
+++ js/loader.js
@@ -1,14 +1,14 @@
 import {doRequest} from './request.js';
 
 function addHeader() {
-  let promise = doRequest("get", "/header.html");
+  let promise = doRequest("get", "header.html");
   promise.then((headerHTML)=>{
     document.body.insertAdjacentHTML("afterbegin", headerHTML);
   });
 }
 
 function addFooter() {
-  let promise = doRequest("get", "/footer.html");
+  let promise = doRequest("get", "footer.html");
   let lastElement = document.body.lastElementChild;
   promise.then((footerHTML)=>{
     console.log(lastElement);
